a15aae2f1ff995f49775dc53d267e865d5a616e4
User1 <110813237+scribecheck@users.noreply.github.com>
2026-08-04 16:56:18 +0900

    Updated files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,4 @@
-const {src, dest, watch, series} = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
-
-function buildStyles() {
-    return src('assets/scss/index.scss')
-        .pipe(sass())
-        .pipe(dest('assets/css'))
-}
-
-function watchTask() {
-    watch(['assets/scss/index.scss'], buildStyles)
-}
-
-exports.default = series(buildStyles, watchTask)
+const sass = require('gulp-sass')(require('sass'));
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
